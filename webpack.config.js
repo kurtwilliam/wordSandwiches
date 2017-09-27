8513a62b0463bee5ad8d7f84e799b44e24dee627
Kurt Schneider <k.w.schneid@gmail.com>
2017-09-26 22:14:07 -0400

    Basic webpack functionality, needs work

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// This library allows us to combine paths easily
+const path = require('path');
+module.exports = {
+   entry: path.resolve(__dirname, 'src/scripts', 'app.js'),
+   output: {
+      path: path.resolve(__dirname, 'public/scripts'),
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [
+         {
+             test: /\.jsx/,
+             use: {
+                loader: 'babel-loader',
+                options: { presets: ['react', 'es2015'] }
+             }
+         },
+         {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+         }
+      ]
+   },
+   devServer: {
+	   contentBase: './src/scripts',
+	   publicPath: '/public/scripts'
+	}
+};
